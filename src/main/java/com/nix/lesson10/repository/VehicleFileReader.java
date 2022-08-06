b4fccdf9ed63c73d6d03821f6241941cbeea74bf
Стас <stas.ruzhytskiy@gamil.com>
2022-08-06 19:08:58 +0300

    io task done

---
+++ src/main/java/com/nix/lesson10/repository/VehicleFileReader.java
@@ -0,0 +1,15 @@
+package com.nix.lesson10.repository;
+
+import com.nix.lesson10.model.vehicle.Truck;
+
+public class VehicleFileReader {
+    public Truck readFromXml(){
+
+        return null;
+    }
+
+    public Truck redaFromJson(){
+        return null;
+    }
+
+}
