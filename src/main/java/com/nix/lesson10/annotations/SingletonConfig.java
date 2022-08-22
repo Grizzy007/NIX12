7fd2338c8050b984c1b2d276e05841879b30ecdc
Стас <stas.ruzhytskiy@gamil.com>
2022-08-22 18:14:35 +0300

    chenges

--- src/main/java/com/nix/lesson10/annotations/SingletonConfig.java
+++ src/main/java/com/nix/lesson10/annotations/SingletonConfig.java
@@ -13,24 +13,11 @@ public class SingletonConfig {
         final Set<Class<? extends VehicleService>> classes = reflections.getSubTypesOf(VehicleService.class);
         reflections = new Reflections("com.nix.lesson10.service");
         final Set<Class<? extends CrudRepository>> repos = reflections.getSubTypesOf(CrudRepository.class);
-        classes.stream()
-                .filter(c -> c.isAnnotationPresent(Singleton.class))
-                .forEach(cl -> {
-                Class<?> bean = cl.getClass();
-                Field[] declaredFields = bean.getDeclaredFields();
-                for (Field declaredField : declaredFields) {
-                    if (declaredField.getName().equals("instance")) {
-                        declaredField.setAccessible(true);
-                        try {
-                            declaredField.set(declaredField, cl.newInstance());
-                            System.out.println("Finish");
-                        } catch (IllegalAccessException | InstantiationException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-        });
-        repos.forEach(cl -> {
+        execute((Set<Class<?>>) repos);
+        execute((Set<Class<?>>) classes);
+    }
+    private void execute(Set<Class<?>> col){
+        col.forEach(cl -> {
             if (cl.isAnnotationPresent(Singleton.class)) {
                 Class<?> bean = cl.getClass();
                 Field[] declaredFields = bean.getDeclaredFields();
