1d48cf4d67f2596deaa4954543af4d1c8366a72b
Стас <stas.ruzhytskiy@gamil.com>
2022-09-05 17:54:36 +0300

    heroku

--- src/main/java/com/nix/lesson10/Main.java
+++ src/main/java/com/nix/lesson10/Main.java
@@ -6,13 +6,13 @@ import org.flywaydb.core.Flyway;
 
 public class Main {
     public static void main(String[] args) {
-        Flyway flyway = Flyway.configure()
-                .dataSource("jdbc:mysql://us-cdbr-east-06.cleardb.net/heroku_798da66bf65e891?reconnect=true",
-                        "baa726347944ec", "bf64f7ff")
-                .baselineOnMigrate(true)
-                .locations("db/migration")
-                .load();
-        flyway.migrate();
+//        Flyway flyway = Flyway.configure()
+//                .dataSource("jdbc:mysql://us-cdbr-east-06.cleardb.net/heroku_798da66bf65e891?reconnect=true",
+//                        "baa726347944ec", "bf64f7ff")
+//                .baselineOnMigrate(true)
+//                .locations("db/migration")
+//                .load();
+//        flyway.migrate();
         UserInterface ui = new UserInterface();
         ui.start();
 
